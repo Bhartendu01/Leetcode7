fa86c1bdd708644468e5c80198d276453774f92e
Saurabh Bhartendu <153103848+Bhartendu01@users.noreply.github.com>
2026-08-08 18:47:34 +0530

    CodePusher: Solution for 167. Two Sum II - Input Array Is Sorted

---
+++ Array/167. Two Sum II - Input Array Is Sorted/167. Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,37 @@
+// Problem: 167. Two Sum II - Input Array Is Sorted
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 19.6 MB (Beats 34.45%)
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         int size = numbers.size();
+//         for(int i=0;i<numbers.size();i++){
+//             for(int j=i+1;j<numbers.size();j++){
+//                 if(numbers[i]== target-numbers[j]){
+//                     return {i+1,j+1};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+class Solution{
+    public:
+    vector<int> twoSum(vector<int>& numbers, int target){
+       int left =0;
+       int right = numbers.size()-1;
+       while(left<right){
+        int sum = numbers[left] + numbers[right];
+        if(sum== target){
+            return {left+1,right+1};
+        }else if(sum<target){
+            left++;
+        }else{
+            right--;
+        }
+       }
+       return {};
+    }
+};
